1674047030e4ae5ffe3a83a2da9742070c12587e
Eduardo David González Bermejo <edudbermejo@gmail.com>
2018-12-21 11:13:16 +0000

    Improve prs list (#3)
    * feat: emojis to me
    
    * feat: added user avatar to PR list

--- commands/list-prs.js
+++ commands/list-prs.js
@@ -11,7 +11,8 @@ const basicSlackPRsMessage = {
 };
 
 const basicPR = {
-  "text": "_url + opened by + openedBy_",
+  "text": "_url_",
+  "author_name": "_openedBy_",
   "callback_id": "update_status",
   "color": "_This depends on the status_",
   "attachment_type": "default",
@@ -40,13 +41,15 @@ const postHiddenMessage = ({ web, skelleton, channel, user }) => {
   web.chat.postEphemeral(response);
 }
 
-const buildPRMessage = prsList => {
+const buildPRMessage = ({prsList, web}) => {
   let finalMessage = Object.assign({}, basicSlackPRsMessage);
   let attachmentsPR = [];
 
   prsList.forEach(prObject => {
     let pullRequest = Object.assign({}, basicPR);
-    pullRequest.text = `${prObject.url} opened by <@${prObject.openedBy}>`;
+    pullRequest.text = `${prObject.url}`;
+    pullRequest.author_name = `<@${prObject.openedBy}>`;
+    pullRequest.author_icon = `${prObject.openedByAvatar}`;
     pullRequest.color = colors[prObject.status];
     let action = Object.assign({}, possibleActions[prObject.status]);
     action.name = prObject.url;
@@ -61,9 +64,9 @@ const buildPRMessage = prsList => {
 exports.listPRs = (web, prsList, message) => {
   
   if (!prsList[message.channel] || prsList[message.channel].length === 0) {
-    postHiddenMessage({ web, skelleton: { text: `There is no opened pull requests in your channel. Seems like you are up to date! Congrats!` }, channel: message.channel, user: message.user });
+    postHiddenMessage({ web, skelleton: { text: `There is no opened pull requests in your channel. Seems like you are up to date! Congrats! :party: :hbcheart:` }, channel: message.channel, user: message.user });
   } else {
-    const answer = buildPRMessage(prsList[message.channel])
+    const answer = buildPRMessage({prsList: prsList[message.channel], web})
     postHiddenMessage({ web, skelleton: answer, channel: message.channel, user: message.user });
   }
 };
