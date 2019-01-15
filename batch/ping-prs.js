d2bb360350cee5cdb7edc13170eab85c2274f30a
edudbermejo <eduardo.gonzalez@hbc.com>
2019-01-15 15:10:19 +0000

    feat: adjusted time for team requirements

--- batch/ping-prs.js
+++ batch/ping-prs.js
@@ -3,8 +3,8 @@ const { listPRs } = require('../commands/list-prs')
 
 let rule = new schedule.RecurrenceRule()
 rule.dayOfWeek = [new schedule.Range(1, 5)]
-rule.hour = [14, 19]
-rule.minute = 0
+rule.hour = [9, 14]
+rule.minute = 30
 // rule.second = [0, 15, 30, 45] //-> for debugging purposes 
 
 exports.setScheduleForPRs = ({web, db}) => {
